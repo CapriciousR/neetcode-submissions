3753d38f6f06e3a543b7b3a13a59bc967464febb
CapriciousR <117285525+CapriciousR@users.noreply.github.com>
2026-08-23 22:18:53 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        curr = []
+
+        def dfs():
+            if len(curr) == len(nums):
+                res.append(curr.copy())
+                return
+        
+            for num in nums:
+                if num in curr:
+                    continue
+                
+                curr.append(num)
+                dfs()
+
+                curr.pop()
+        
+        dfs()
+
+        return res
