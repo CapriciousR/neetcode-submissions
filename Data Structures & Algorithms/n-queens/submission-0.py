e37b0088371d70a3d081d91e1bbb594c61080439
CapriciousR <117285525+CapriciousR@users.noreply.github.com>
2026-08-25 14:22:20 +0530

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.py
@@ -0,0 +1,55 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        if not n:
+            return []
+
+        res = []
+
+        curr = []
+
+        r = set()
+        c = set()
+        d1 = set()
+        d2 = set()
+        for i in range(n):
+            r.add(i)
+            c.add(i)
+            d1.update([-i,i])
+            d2.update([i,i+n-1])
+        
+        def solve(i):
+            if i == n:
+                res.append(curr.copy())
+                return
+            
+            for j in range(n):
+                if j in c and i-j in d1 and i+j in d2:
+                    curr.append((i,j))
+                    c.remove(j)
+                    d1.remove(i-j)
+                    d2.remove(i+j)
+                    solve(i+1)
+                    d2.add(i+j)
+                    d1.add(i-j)
+                    c.add(j)
+                    curr.pop()
+            
+        solve(0)
+        
+        ans = []
+
+        for sol in res:
+            solu = []
+            for i,j in sol:
+                curr = ""
+                for k in range(n):
+                    if k == j:
+                        curr += "Q"
+                    else:
+                        curr += "."
+                solu.append(curr)
+            ans.append(solu)
+
+
+        return ans
+                
